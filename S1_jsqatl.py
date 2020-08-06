b1ceaa994f5f6e26749dcb9dfbd814702fd43bbd
jessqatl <40494660+jessqatl@users.noreply.github.com>
2020-08-06 03:41:35 -0400

    Add files via upload

---
+++ S1_jsqatl.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Import Libraries 
+import random, math
+import numpy as np
+import scipy as sp
+import scipy.stats as stats 
+import matplotlib.pyplot as plt 
+import SimPy.Simulation as Sim
+
+
+# In[ ]:
+
+
+#parameters
+x = X/S
+zw = Zw / S 
+a = A / S
+z0 = Z0 / S
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
